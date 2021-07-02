4164d8f93ec722e9cb2bdd8985a933f38654dbdf
Maryann878 <85343201+Maryann878@users.noreply.github.com>
2021-07-02 10:56:47 +0100

    Add files via upload

--- week5/script.js
+++ week5/script.js
@@ -27,11 +27,12 @@ users.map(function(val){
 
 
     for(var i = 0; i <= 15; i++)
-    {
-        console.log(i + 'ís even')
+    {if (i===0){
+        console.log(i + 'ís even');
     }
     else if (i % 2 ===0) {
-        console.log(i + 'is even')
+
+        console.log(i + 'is even');}
         
         else {console.log(i + 'is odd')}
     }
